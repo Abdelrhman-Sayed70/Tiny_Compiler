e783d88f389e598a30967357dcda98573f942d80
Abdo sayed <abdulrhmansayed70@gmail.com>
2022-12-26 21:20:16 +0200

    update

--- MS 2/Tiny Compiler Project [PARSER]/JASON_Compiler/Form1.Designer.cs
+++ MS 2/Tiny Compiler Project [PARSER]/JASON_Compiler/Form1.Designer.cs
@@ -57,7 +57,7 @@
             this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(774, 346);
+            this.textBox1.Size = new System.Drawing.Size(519, 346);
             this.textBox1.TabIndex = 0;
             // 
             // textBox2
